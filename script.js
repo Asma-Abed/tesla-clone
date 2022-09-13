647297e880e558dade86f4a5fa854afd19b31f7b
Asma-Abed <abed.asma@hotmail.com>
2022-09-13 13:38:23 +0100

    opacity effect added to sections

--- script.js
+++ script.js
@@ -1,15 +1,131 @@
+const model1Section = document.querySelector('.model-1-section');
+const model2Section = document.querySelector('.model-2-section');
+const model3Section = document.querySelector('.model-3-section');
+const model4Section = document.querySelector('.model-4-section');
+const house1Section = document.querySelector('.house-1-section');
+const house2Section = document.querySelector('.house-2-section');
 const lastSection = document.querySelector('.last-section');
+
+const model1Content = document.querySelector('.model-1-content');
+const model2Content = document.querySelector('.model-2-content');
+const model3Content = document.querySelector('.model-3-content');
+const model4Content = document.querySelector('.model-4-content');
+const house1Content = document.querySelector('.house-1-content');
+const house2Content = document.querySelector('.house-2-content');
+const connectorContent = document.querySelector('.connector-content');
 const footer = document.querySelector('.footer');
+
+const showfirstModel = (entries) => {
+  const [entry] = entries;
+  console.log(entry);
+  if (entry.isIntersecting) {
+    model1Content.classList.add('show');
+  } else {
+    model1Content.classList.remove('show');
+  }
+};
+
+const firstModelObserver = new IntersectionObserver(showfirstModel, {
+  root: null,
+  threshold: 0.6,
+});
+
+firstModelObserver.observe(model1Section);
+
+const showsecModel = (entries) => {
+  const [entry] = entries;
+  console.log(entry);
+  if (entry.isIntersecting) {
+    model2Content.classList.add('show');
+  } else {
+    model2Content.classList.remove('show');
+  }
+};
+
+const secModelObserver = new IntersectionObserver(showsecModel, {
+  root: null,
+  threshold: 0.6,
+});
+
+secModelObserver.observe(model2Section);
+
+const showThirdModel = (entries) => {
+  const [entry] = entries;
+  console.log(entry);
+  if (entry.isIntersecting) {
+    model3Content.classList.add('show');
+  } else {
+    model3Content.classList.remove('show');
+  }
+};
+
+const thirdModelObserver = new IntersectionObserver(showThirdModel, {
+  root: null,
+  threshold: 0.6,
+});
+
+thirdModelObserver.observe(model3Section);
+
+const showforthModel = (entries) => {
+  const [entry] = entries;
+  console.log(entry);
+  if (entry.isIntersecting) {
+    model4Content.classList.add('show');
+  } else {
+    model4Content.classList.remove('show');
+  }
+};
+
+const forthModelObserver = new IntersectionObserver(showforthModel, {
+  root: null,
+  threshold: 0.6,
+});
+
+forthModelObserver.observe(model4Section);
+
+const showfirstHouse = (entries) => {
+  const [entry] = entries;
+  console.log(entry);
+  if (entry.isIntersecting) {
+    house1Content.classList.add('show');
+  } else {
+    house1Content.classList.remove('show');
+  }
+};
+
+const firstHouseObserver = new IntersectionObserver(showfirstHouse, {
+  root: null,
+  threshold: 0.6,
+});
+
+firstHouseObserver.observe(house1Section);
+
+const showSecHouse = (entries) => {
+  const [entry] = entries;
+  console.log(entry);
+  if (entry.isIntersecting) {
+    house2Content.classList.add('show');
+  } else {
+    house2Content.classList.remove('show');
+  }
+};
+
+const secHouseObserver = new IntersectionObserver(showSecHouse, {
+  root: null,
+  threshold: 0.6,
+});
+
+secHouseObserver.observe(house2Section);
 
 const showFooter = (entries) => {
   const [entry] = entries;
   console.log(entry);
   if (entry.isIntersecting) {
-    footer.style.opacity = 1;
-    footer.style.visibility = 'visible';
+    footer.classList.add('show');
+    connectorContent.classList.add('show');
   } else {
-    footer.style.opacity = 0;
-    footer.style.visibility = 'hidden';
+    footer.classList.remove('show');
+    connectorContent.classList.remove('show');
   }
 };
 
